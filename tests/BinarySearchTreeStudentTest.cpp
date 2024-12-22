e5884bffa357f9775dc54bd76eaa1cfff101fccb
breadmayn <brandonmachh@gmail.com>
2024-12-22 18:05:08 -0500

    initial setup

---
+++ tests/BinarySearchTreeStudentTest.cpp
@@ -0,0 +1,16 @@
+#include <gtest/gtest.h>
+#include <chrono>
+#include <future>
+
+#include "../src/BinarySearchTree.hpp"
+
+struct BinarySearchTreeStudentTest : public ::testing::Test {
+
+    virtual void SetUp() override {
+
+    }
+
+    virtual void TearDown() override {
+
+    }
+};
